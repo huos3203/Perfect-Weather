998e4dc3c8527ea3734c84d1459cdb3595fb6bfb
iTBoyer <itboyer@qq.com>
2019-11-29 14:46:14 +0800

    升级版本工具

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "Perfect-Weather",
-    targets: [],
     dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2, minor: 0),
-		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0, minor: 1),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", majorVersion: 2, minor: 0)
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from:"3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", from:"1.0.0"),
+		.package(url: "https://github.com/iamjono/JSONConfig.git", from: "1.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-libcurl.git", from:"1.0.0")
 	]
 
 )
